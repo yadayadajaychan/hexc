2ee0dc03e75d94aece7105852bf70e58b9a26cfa
Ethan Cheng <ethanrc0528@gmail.com>
2022-08-30 15:34:58 -0700

    added rudimentary number entry, multiplication, and addition

--- src/main.c
+++ src/main.c
@@ -5,12 +5,73 @@ int main(void)
 {
 	init_screen();
 
-	while (!os_GetCSC());
+	uint8_t key;
+	uint64_t last_reg_x = 0;
+	extern int should_stack_lift;
 
-	///gfx_SetTextXY(16, 18);
-	display_int(10000, 'h');
+	while (true) {
 
-	while (!os_GetCSC());
+		if (boot_CheckOnPressed())
+			break;
+
+		key = os_GetCSC();
+
+		switch (key) {
+		case sk_0:
+			push(0);
+			break;
+		case sk_1:
+			push(1);
+			break;
+		case sk_2:
+			push(2);
+			break;
+		case sk_3:
+			push(3);
+			break;
+		case sk_4:
+			push(4);
+			break;
+		case sk_5:
+			push(5);
+			break;
+		case sk_6:
+			push(6);
+			break;
+		case sk_7:
+			push(7);
+			break;
+		case sk_8:
+			push(8);
+			break;
+		case sk_9:
+			push(9);
+			break;
+		case sk_Add:
+			should_stack_lift = true;
+			push(pop() + pop());
+			should_stack_lift = true;
+			break;
+		case sk_Mul:
+			should_stack_lift = true;
+			push(pop() * pop());
+			should_stack_lift = true;
+			break;
+		case sk_Enter:
+			lift_stack();
+			should_stack_lift = false;
+			break;
+		default:
+			break;
+		}
+
+		/* display the contents of register x */
+		if (last_reg_x != get_reg_x()) {
+			last_reg_x = get_reg_x();
+			display_int(last_reg_x, 'd');
+		}
+	}
+
 	end_screen();
 	return 0;
 }
